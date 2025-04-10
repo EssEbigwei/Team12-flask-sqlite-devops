6b2ab2c76139b5bae81b817beb3ad16cd2e4b2a4
Mors91 <snrwhyte91@yahoo.com>
2025-04-09 20:14:51 -0400

    updated sql table

--- init_db.sql
+++ init_db.sql
@@ -1,6 +1,10 @@
-CREATE TABLE messages (
+CREATE TABLE IF NOT EXISTS messages (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  message TEXT
+  message TEXT UNIQUE
 );
 
-INSERT INTO messages (message) VALUES ('Hello from SQLite!'), ('Deployed with Ansible'), ('Artifact stored in S3');
+INSERT OR IGNORE INTO messages (message) 
+VALUES 
+  ('Hello from SQLite!'), 
+  ('Deployed with Ansible'), 
+  ('Artifact stored in S3');
